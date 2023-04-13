f38aa712701231e6c328237a66f44be14bc24488
avrians <avriansyahb10@gmail.com>
2023-04-13 16:24:44 +0700

    update

--- app/src/main/java/com/plugin/projectshowcase/ui/screen/Home.kt
+++ app/src/main/java/com/plugin/projectshowcase/ui/screen/Home.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.plugin.projectshowcase.data.api.model.Photos
 import com.plugin.projectshowcase.viewModel.HomeViewModel
 
 @Composable
@@ -26,9 +29,17 @@ fun Home () {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-
+                    CircularProgressIndicator()
                 }
             }
         }
+         items(state) { photos : Photos ->
+
+         }
     }
+}
+
+@Composable
+fun PhotosListItem( photos : Photos ) {
+
 }
